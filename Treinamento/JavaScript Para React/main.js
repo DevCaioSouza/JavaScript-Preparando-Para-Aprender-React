ce2285b9328419b9b044d8373747a15db622608b
unknown <caiocbahia@hotmail.com>
2022-08-30 17:00:34 -0300

    nullish coalescing concluído

--- Treinamento/JavaScript Para React/main.js
+++ Treinamento/JavaScript Para React/main.js
@@ -1,1 +1,15 @@
-document.body.innerText = 'Olá mundo'
+//O nullish coalescing operator é pra fazer com que o zero 
+//não seja interpretado como valor nulo pelo JavaScript
+
+//lembrando que os seguintes valores são considerados não válidos
+// 0, '', [], false, undefined, null => 'falsy'
+
+const idade = 0;
+
+//dessa forma com || mesmo se colocado valor zero continua 
+//passando não informado
+// document.body.innerText = 'A idade de Caio é ' + (idade || 'Não Informado' );
+
+//porém se usarmos o nullish coalescing operator, o zero será valor válido
+document.body.innerText = 'A idade de Caio é ' + (idade ?? 'Não Informado' );
+

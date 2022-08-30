1eac27a2628f089b561d89e658f25745e9b978cc
unknown <caiocbahia@hotmail.com>
2022-08-30 18:23:49 -0300

    desestruturação concluído

--- Treinamento/JavaScript Para React/main.js
+++ Treinamento/JavaScript Para React/main.js
@@ -23,7 +23,42 @@ const jogador = {
 //Object.entries() retorna um vetor com vários vetores dentro
 //cada um dos subvetores do vetor maior tem duas informações dentro,
 //no caso a chave e o valor do objeto
-document.body.innerText = JSON.stringify(Object.entries(jogador));
+// document.body.innerText = JSON.stringify(Object.entries(jogador));
+
+//------------------------------------------------------------------------
+//Desestruturação
+//Remover parte do objeto p/ uma variável a parte
+
+// const origem = jogador.origem
+//podemos fazer tambem:
+// const {origem, idade} = jogador;
+
+//podemos também renomear os atributos
+// const {origem: origin, idade: age} = jogador
+//não podemos esquecer de passar o novo parâmetro na chamada tb
+
+//tb conseguimos atribuir valores padrão para variáveis
+//o "apelido" só será exibido caso ele não conste no objeto original
+// const {origem: origin, idade: age, apelido = 'Baixinho'} = jogador
+// document.body.innerText = JSON.stringify({origin, age, apelido})
+
+//Desestruturação pode ser usado em qualquer lugar que utilize objetos, 
+//como parâmetro de funções, variáveis, consultores, métodos de classes, etc;
+// function mostraIdade(jogador){
+//   return jogador.idade
+// }
+
+//desestruturando o parâmetro
+function mostraIdade({idade}){
+  return idade
+}
+
+document.body.innerText = mostraIdade(jogador)
+
+
+
+
+
 
 
 

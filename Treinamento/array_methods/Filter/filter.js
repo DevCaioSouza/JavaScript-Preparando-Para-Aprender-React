dea57a1f3d6d764347f2b632f7e1a124f83c1757
unknown <caiocbahia@hotmail.com>
2022-08-31 09:48:05 -0300

    Map condicional

---
+++ Treinamento/array_methods/Filter/filter.js
@@ -0,0 +1,3 @@
+//Filter apenas filtra o array, e não altera seus elementos
+
+const array = [1, 2, 3, 4, 5];
--- Treinamento/array_methods/Map/map.js
+++ Treinamento/array_methods/Map/map.js
@@ -14,10 +14,19 @@ const array = [1, 2, 3, 4, 5];
 //modificados, por multiplicação ou divisão por exemplo
 
 
+// const novoArray = array.map(item => {
+//   return item * 2;
+// });
+
 const novoArray = array.map(item => {
-  return item * 2;
-});
+  if (item % 2 === 0){
+    return item * 10
+  }
+
+  return item
+})
 
 document.body.innerText = JSON.stringify(novoArray)
 
 //o map sempre retorna um array do tamanho do original
+

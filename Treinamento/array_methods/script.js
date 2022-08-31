42132c19862c81da79d47f1f433e09366c2c909e
unknown <caiocbahia@hotmail.com>
2022-08-31 01:51:30 -0300

    map ex 1

---
+++ Treinamento/array_methods/script.js
@@ -0,0 +1,23 @@
+const array = [1, 2, 3, 4, 5];
+
+//usando forEach
+
+// const novoArray = [];
+
+// array.forEach(item => {
+//   novoArray.push(item * 2);
+// })
+
+//-------------------------------------------
+
+//Diferente do forEach, o map pode retornar os itens do array
+//modificados, por multiplicação ou divisão por exemplo
+
+
+const novoArray = array.map(item => {
+  return item * 2;
+});
+
+document.body.innerText = JSON.stringify(novoArray)
+
+//o map sempre retorna um array do tamanho do original

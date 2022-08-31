b41f3d2803030e8ca543c4a0297f7249b48404db
unknown <caiocbahia@hotmail.com>
2022-08-31 19:04:20 -0300

    pegando api do github com fetch e promises

--- Treinamento/Promises/script.js
+++ Treinamento/Promises/script.js
@@ -5,18 +5,43 @@
 
 //-----Exemplo_01-----
 
-const somaNumeros = (a, b) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() =>{
-      resolve(a + b);
-    }, 2000);
-  })
-}
+// const somaNumeros = (a, b) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() =>{
+//       resolve(a + b);
+//     }, 2000);
+//   })
+// }
 
-somaNumeros(1, 3)
-  .then(soma => {
-    document.body.innerText = soma
+// somaNumeros(1, 3)
+//   .then(soma => {
+//     document.body.innerText = soma
+//   })
+//   .catch (err => {
+//     console.log(err)
+//   })
+
+//-----Exemplo_02-----
+
+fetch('https://api.github.com/users/manoCaioZ')
+  .then(res => {
+    res.json()
+  .then(body => {
+    console.log(body)
+    })
   })
-  .catch (err => {
+  .catch(err =>{
     console.log(err)
   })
+  .finally(() =>{
+    console.log('deu tudo certo')
+  })
+
+//res.text()-> o text() é uma promise, que irá retornar uma string
+//podemos tambem usar json()
+
+//vamos chamar o alinhamento dos .then() de tratativa da promessa
+//o primeiro trata do formato da resposta, o subsequente utiliza
+//esses dados de alguma forma
+
+//o finally sempre será executado independente do sucesso ou não da promessa

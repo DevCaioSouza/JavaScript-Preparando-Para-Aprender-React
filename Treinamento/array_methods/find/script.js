a25fe234829269322ac28d679cd80da7dfc2939a
unknown <caiocbahia@hotmail.com>
2022-09-01 18:58:37 -0300

    find e findIndex - exemplos

--- Treinamento/array_methods/find/script.js
+++ Treinamento/array_methods/find/script.js
@@ -2,6 +2,14 @@ const array = [1, 2, 3, 4, 5]
 
 const pares = array.find(item => item % 2 === 0);
 
-document.body.innerText = JSON.stringify(pares)
+// document.body.innerText = JSON.stringify(pares)
 
 //o find retorna apenas o primeiro item que case com a descrição
+//depois de percorrer o array do começo ao fim
+
+//para achar o índice aonde o elemento foi encontrado 
+//utilizamos findIndex
+
+const parIndex = array.findIndex(item => item % 2 === 0)
+
+document.body.innerText = JSON.stringify(parIndex)

710a01f80e550dd2a5fef73ee5d41bae82aced15
unknown <caiocbahia@hotmail.com>
2022-09-01 10:17:17 -0300

    find - exemplo simples

---
+++ Treinamento/array_methods/find/script.js
@@ -0,0 +1,7 @@
+const array = [1, 2, 3, 4, 5]
+
+const pares = array.find(item => item % 2 === 0);
+
+document.body.innerText = JSON.stringify(pares)
+
+//o find retorna apenas o primeiro item que case com a descrição
